34a3fe4de1450a59185ea2450d79ecad26180c31
Christopher Totlyakov <laminex0622@gmail.com>
2025-06-19 21:14:11 +0300

    add & setup handlebars & home page + layouts

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import express from 'express'
 import routes from './routes.js';
+import handlebars from 'express-handlebars'
 
 const app = express();
 
 app.use(express.static('src/public'));
 
 app.use(express.urlencoded());
- 
+
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+    runtimeOptions: {
+        allowProtoMethodsByDefault: true,
+        allowProtoPropertiesByDefault: true,
+        // allowedProtoMethods:{ security
+        //     'getAll': true
+        // }
+    }
+}));
+app.set('view engine', 'hbs');
+app.set('views', 'src/views')
+
 app.use(routes);
 
 app.listen(3000,() => console.log('server is listening on http://localhost:3000'));
